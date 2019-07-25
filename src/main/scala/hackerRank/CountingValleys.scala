0bed2a80bb82a2b5ff490040c9131941965d36bc
Clairton Menezes <clairton.menezes@gmail.com>
2019-07-24 22:59:29 -0300

    Added: - Sock Merchant resolution - Counting Valleys Exercise

---
+++ src/main/scala/hackerRank/CountingValleys.scala
@@ -0,0 +1,12 @@
+package hackerRank
+
+object CountingValleys {
+
+  def countValley(n: Int, Path: String): Int = {
+    ???
+  }
+
+  val pathTest = "UDDDUDUU"
+  val pathTest2 = "UDDDUUDDDUUU"
+
+}
